8a9a79924199484917e5147441887774bad4b9b4
erhimanshurajak <103110823+erhimanshurajak@users.noreply.github.com>
2026-05-19 12:36:23 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,24 +1,60 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-064ff912f78e3e561"
-  instance_type = "t2.micro"
+# Create VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  tags = {
-    Name = "himansh"
+# Create Subnets
+resource "aws_subnet" "eks_subnet_a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "eks_subnet_b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+# Create IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach EKS Policy to IAM Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create EKS Cluster
+resource "aws_eks_cluster" "example" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.eks_subnet_a.id,
+      aws_subnet.eks_subnet_b.id,
+    ]
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+  ]
 }
